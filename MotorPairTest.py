bb43494e0177a96f2a728594c4e7f8f71dbfef7a
gpetitpas <gpetitpas@users.noreply.github.com>
2023-12-30 17:44:57 -0500

    Test MotorPair

---
+++ MotorPairTest.py
@@ -0,0 +1,14 @@
+from buildhat import MotorPair
+import time
+import math
+
+# https://buildhat.readthedocs.io/en/latest/buildhat/motor.html
+
+# Next up:
+# get diameter of the wheel, program to drive a certain distance forward/backward
+# get width of robot, program to turn in place
+
+pair = MotorPair('A', 'B')
+pair.set_default_speed(70)
+
+pair.run_for_rotations(2)
