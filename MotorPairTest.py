27d5ab2acb5424bf2f754cb7f7ef376773e5c8c1
gpetitpas <gpetitpas@users.noreply.github.com>
2023-12-31 13:44:02 -0500

    LegoMotorPair

--- MotorPairTest.py
+++ MotorPairTest.py
@@ -1,6 +1,7 @@
 from buildhat import MotorPair
 import time
 import math
+from LegoMotorPair import LegoMotorPair
 
 # https://buildhat.readthedocs.io/en/latest/buildhat/motor.html
 
@@ -8,8 +9,22 @@ import math
 # get diameter of the wheel, program to drive a certain distance forward/backward
 # get width of robot, program to turn in place
 
-pair = MotorPair('A', 'B')
-pair.set_default_speed(70)
+# pair = MotorPair('A', 'B')
+# pair.set_default_speed(70)
 
-pair.run_for_rotations(2)
-print('hi')
+# pair.run_for_rotations(2)
+# print('hi')
+# pair.start(-70, 70)
+# print('hi')
+# time.sleep(2)
+# pair.stop()
+# print('end')
+
+pair = LegoMotorPair('A', 'B', 70)
+pair.start()
+print('should convert to')
+time.sleep(2)
+pair.start(-20, 20)
+print('hi 20')
+time.sleep(2)
+print('end')
