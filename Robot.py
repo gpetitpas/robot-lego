68878a9d799e4e9ed388874cb1275d4a2e8a4c5c
gpetitpas <gpetitpas@users.noreply.github.com>
2024-01-05 16:42:15 -0500

    can only use motor, not motor pair

--- Robot.py
+++ Robot.py
@@ -13,42 +13,44 @@ ticks_to_mm = (math.pi / ticks_per_revolution) * wheel_diameter
 
 class Robot:
     def __init__(self) -> None:
-        self.pair = LegoMotorPair('A', 'B', 70)
+        # self.pair = LegoMotorPair('A', 'B', 70)
         self.dist_sensor = LegoDistanceSensor('D')
         self.left_motor = LegoMotor('A')
         self.right_motor = LegoMotor('B')
 
 
     def start(self, left_speed=-70, right_speed=70):
-        self.pair.start(left_speed, right_speed)
+        self.left_motor.start(left_speed)
+        self.right_motor.start(right_speed)
 
     def stop(self):
-        self.pair.stop()
+        self.left_motor.stop()
+        self.right_motor.stop()
 
     # For encoder ticks, use the output from motor.get_position()
     def ticks_to_mm(self, encoder_ticks):
         return int(ticks_to_mm * encoder_ticks)
 
-    def drive_straight(self, seconds):
-        self.pair.start()
-        time.sleep(seconds)
-        self.pair.stop()
-        time.sleep(0.1)
+    # def drive_straight(self, seconds):
+    #     self.pair.start()
+    #     time.sleep(seconds)
+    #     self.pair.stop()
+    #     time.sleep(0.1)
 
-    def turn_left(self):
-        self.pair.start(50, 50)
-        time.sleep(0.45)
-        self.pair.stop()
-        time.sleep(0.1)
+    # def turn_left(self):
+    #     self.pair.start(50, 50)
+    #     time.sleep(0.45)
+    #     self.pair.stop()
+    #     time.sleep(0.1)
 
-    def turn_right(self):
-        self.pair.start(-50, -50)
-        time.sleep(0.45)
-        self.pair.stop()
-        time.sleep(0.1)
+    # def turn_right(self):
+    #     self.pair.start(-50, -50)
+    #     time.sleep(0.45)
+    #     self.pair.stop()
+    #     time.sleep(0.1)
 
-    def spin_clockwise(self):
-        self.pair.start(-50, -50)
-        time.sleep(1.8)
-        self.pair.stop()
-        time.sleep(0.1)
+    # def spin_clockwise(self):
+    #     self.pair.start(-50, -50)
+    #     time.sleep(1.8)
+    #     self.pair.stop()
+    #     time.sleep(0.1)
